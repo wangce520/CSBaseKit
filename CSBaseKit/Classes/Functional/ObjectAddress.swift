6c0c9550512625cc23a850df9af57d25ca4f8070
王策 <ce.wang@etocrm.com>
2023-03-09 09:37:16 +0800

    feat: 修改地址获取类

--- CSBaseKit/Classes/Functional/ObjectAddress.swift
+++ CSBaseKit/Classes/Functional/ObjectAddress.swift
@@ -7,18 +7,21 @@
 
 import Foundation
 
-extension AnyObject {
+class ObjectHelper {
     
     // 对比两个对象地址是否相等
-    func equalTo(_ object : AnyObject) -> Bool {
-        let str1 = object.getMemoryAddress()
-        let str2 = getMemoryAddress()
+    static func equatableAnyObject(_ object1 : AnyObject, _ object2 : AnyObject) -> Bool {
+        let str1 = getAnyObjectMemoryAddress(object1)
+        let str2 = getAnyObjectMemoryAddress(object2)
         return str1 == str2
     }
 
     // 获取对象地址
-    func getMemoryAddress() -> String {
-        let str = Unmanaged<AnyObject>.passUnretained(self).toOpaque()
+    static func getAnyObjectMemoryAddress(_ object : AnyObject) -> String {
+        let str = Unmanaged<AnyObject>.passUnretained(object).toOpaque()
         return String(describing: str)
     }
+
 }
+
+
